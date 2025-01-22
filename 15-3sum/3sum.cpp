fc724844b5cbb3248c5f1def46ee7784b82f3625
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-01-23 00:16:37 +0530

    Time: 55 ms (43.94%) | Memory: 31 MB (31.62%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -28,37 +28,73 @@ public:
 
    // better solution
 
-     unordered_map<int,int>m;
-      set<vector<int>>s;
-      vector<vector<int>>ans;
-     for(int i=0;i<nums.size();i++)
-     {
-        m[nums[i]]=i;
-     }
+    
+    //   set<vector<int>>s;
+    //   vector<vector<int>>ans;
+    //  for(int i=0;i<nums.size();i++)
+    //  {
+    //      unordered_map<int,int>m;
+    //     for(int j=i+1;j<nums.size();j++)
+    //     {
+    //         int searchInMap=-(nums[i]+nums[j]);
 
-     for(int i=0;i<nums.size();i++)
-     {
-        for(int j=i+1;j<nums.size();j++)
+    //         if(m.find(searchInMap)!=m.end())
+    //         {
+    //             vector<int>triplet={nums[i],nums[j],searchInMap};
+    //             sort(triplet.begin(),triplet.end());
+    //             s.insert(triplet);
+                
+    //         }
+    //         m[nums[j]]++;
+    //     }
+    //  }
+    //   for(auto i:s)
+    //   {
+    //     ans.push_back(i);
+    //   }
+    //   return ans;
+
+
+vector<vector<int>>ans;
+sort(nums.begin(),nums.end());
+for(int i=0;i<nums.size();i++)
+{
+      if (i != 0 && nums[i] == nums[i - 1]) continue;
+
+    int j=i+1,k=nums.size()-1;
+
+    while(j<k)
+    {
+        int target=-nums[i];
+        int sum=nums[j]+nums[k];
+        if(sum>target)
         {
-            int searchInMap=-(nums[i]+nums[j]);
-
-            if(m.find(searchInMap)!=m.end())
-            {
-                int index=m[searchInMap];
-                if(index>i&&index>j)
-                {
-                    vector<int>triplet={nums[i],nums[j],searchInMap};
-                        sort(triplet.begin(),triplet.end());
-                         s.insert(triplet);
-                }
-            }
+           
+           k--;
         }
-     }
-      for(auto i:s)
-      {
-        ans.push_back(i);
-      }
-      return ans;
+        else if(sum<target)
+        {
+           
+              j++;
+        }
+        else
+        {
+          vector<int>triplets={nums[i],nums[j],nums[k]};
+          ans.push_back(triplets);
+          j++,k--;
+           while(j<k&&nums[j]==nums[j-1])
+           {
+              j++;
+           }
+           while(j<k&&nums[k]==nums[k+1])
+           {
+            k--;
+           }
+
+        }
+    }
+}
+return ans;
 
     }
 };
