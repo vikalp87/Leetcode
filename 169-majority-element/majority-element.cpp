942b863827321c20db89478c255b340601a2ddcd
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-01-23 23:48:59 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (23.85%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,30 +1,46 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-       sort(nums.begin(),nums.end());
-       int i,count=0;
-       int n=nums.size();
-       for(i=0;i<nums.size();i++)
-       {
-            if(i+1<nums.size()&&nums[i]==nums[i+1])
-            count++;
-            else
-            {
-                if((count+1)>(n/2))
-               {
-                   return nums[i];
-               }
+    //    sort(nums.begin(),nums.end());
+    //    int i,count=0;
+    //    int n=nums.size();
+    //    for(i=0;i<nums.size();i++)
+    //    {
+    //         if(i+1<nums.size()&&nums[i]==nums[i+1])
+    //         count++;
+    //         else
+    //         {
+    //             if((count+1)>(n/2))
+    //            {
+    //                return nums[i];
+    //            }
 
-                count=0;
-            }
-       }
-             if((count+1)>(n/2))
-               {
-                   return nums[i];
-               }
+    //             count=0;
+    //         }
+    //    }
+    //          if((count+1)>(n/2))
+    //            {
+    //                return nums[i];
+    //            }
 
        
 
-    return 0;
+    // return 0;
+
+       unordered_map<int,int>m;
+       for(int i=0;i<nums.size();i++)
+       {
+        m[nums[i]]++;
+       }
+       for(auto i:m)
+       {
+          if(i.second>(nums.size()/2))
+          {
+            return i.first;
+          }
+       }
+
+   return 0;
+   
     }
 };
