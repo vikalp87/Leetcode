ed83c6cb94ce182f0c99cb4f9f95246656b04519
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-01-23 22:41:52 +0530

    Time: 19 ms (79.85%) | Memory: 17.4 MB (60.39%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,42 +1,89 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>>ans;
-        set<vector<int>>s;
+      // tc=n^3(logn+logm) where logn is while find and inserting in set(ans) and log m is no  
+      // of unique element present in set.
+      // sc=o(2*no of quaduplets)+o(n)(at worst case set store n-3 eleement)
+        // vector<vector<int>>ans;
+        // set<vector<int>>s;
 
-        for(int i=0;i<nums.size();i++)
-        {
+        // for(int i=0;i<nums.size();i++)
+        // {
            
-            for(int j=i+1;j<nums.size();j++)
-            {
-                  set<long long>m;
-                for(int k=j+1;k<nums.size();k++)
-                {
-                  long long fourthNo=target-nums[i];
-                  fourthNo-=nums[j];
-                  fourthNo-=nums[k];
+        //     for(int j=i+1;j<nums.size();j++)
+        //     {
+        //           set<long long>m;
+        //         for(int k=j+1;k<nums.size();k++)
+        //         {
+        //           long long fourthNo=target-nums[i];
+        //           fourthNo-=nums[j];
+        //           fourthNo-=nums[k];
 
-                   if(m.find(fourthNo)!=m.end())
-                   {
+        //            if(m.find(fourthNo)!=m.end())
+        //            {
                   
-                       vector<int>result={nums[i],nums[j],nums[k],int(fourthNo)};
-                       sort(result.begin(),result.end());
-                       s.insert(result);
+        //                vector<int>result={nums[i],nums[j],nums[k],int(fourthNo)};
+        //                sort(result.begin(),result.end());
+        //                s.insert(result);
                        
-                   }
+        //            }
                    
-                   m.insert(nums[k]);
+        //            m.insert(nums[k]);
 
 
-                }
+        //         }
+        //     }
+        // }
+
+        //    for(auto i:s)
+        //    {
+        //      ans.push_back(i);
+        //    }
+        // return ans;
+
+      vector<vector<int>>ans;
+      sort(nums.begin(),nums.end());
+      if(nums.size()<4)
+      return ans;
+      
+      for(int i=0;i<nums.size();i++)
+      {
+        if(i>0&&nums[i]==nums[i-1])continue;
+        for(int j=i+1;j<nums.size();j++)
+        {
+             if((j-i>1)&&nums[j]==nums[j-1])continue;
+
+            int k=j+1,l=nums.size()-1;
+            long  reqsum=target-nums[i];
+           reqsum-= nums[j];
+            while(k<l)
+            {
+                 if(reqsum>(nums[k]+nums[l]))
+                 {
+                      k++;
+                 }
+                 else if(reqsum<(nums[k]+nums[l]))
+                 {
+                      l--;
+                 }
+                 else
+                 {
+                    vector<int>result={nums[i],nums[j],nums[k],nums[l]};
+                    ans.push_back(result);
+                   
+                    k++,l--;
+                      while(k<l&&nums[k]==nums[k-1])
+                      k++;
+                      while(k<l&&nums[l]==nums[l+1])
+                      {
+                        l--;
+                      }
+                 }
             }
         }
+      }
 
-           for(auto i:s)
-           {
-             ans.push_back(i);
-           }
-        return ans;
+return ans;
         
     }
 };
