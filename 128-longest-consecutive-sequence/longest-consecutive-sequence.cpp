c9c2d9abce840f329ce0819b37cb9ef0c5e8e0fb
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-01-27 22:41:00 +0530

    Time: 82 ms (47.54%) | Memory: 87.8 MB (11.41%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,30 +1,60 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-       int count=1;
-       int maxi=0;
-       int n=nums.size();
-sort(nums.begin(),nums.end());
-       for(int i=0;i<n;i++)
-       {
-           if(i+1>1&&abs(nums[i]-nums[i-1]==1))
-            {
-                count++;
-                maxi=max(maxi,count);
-            }
-            else
-            {
-                if(i+1<=1)
-                {
-                    maxi=max(maxi,count);
-                }
+//       n int count=1;
+//        int maxi=0;
+//        int n=nums.size();
+// sort(nums.begin(),nums.end());
+//        for(int i=0;i<n;i++)
+//        {
+//            if(i+1>1&&abs(nums[i]-nums[i-1]==1))
+//             {
+//                 count++;
+//                 maxi=max(maxi,count);
+//             }
+//             else
+//             {
+//                 if(i+1<=1)
+//                 {
+//                     maxi=max(maxi,count);
+//                 }
 
-                if(i+1>1&&nums[i]-nums[i-1]!=0)
-                   count=1;
-            }
+//                 if(i+1>1&&nums[i]-nums[i-1]!=0)
+//                    count=1;
+//             }
 
             
-       }
-           return maxi;
+//        }
+//            return maxi;
+
+
+      int count=1;
+      int maxi=0;
+      unordered_set<int>s;
+      for(int i=0;i<nums.size();i++)
+      {
+        s.insert(nums[i]);
+      }
+      // calculate maximum posiible sequence for one interation
+    
+      for(int i:s)
+      {
+        if(!(s.find(i-1)!=s.end()))
+        {
+        int ele=i+1;
+        while(s.find(ele)!=s.end())
+        {
+            count++;
+           
+            ele++;
+               
+        }
+        maxi=max(maxi,count);
+         count=1;
+        }
+        
+      }
+
+return maxi;
     }
 };
