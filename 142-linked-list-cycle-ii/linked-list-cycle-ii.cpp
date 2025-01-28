7eb7005c01313a2754f21f3aebe7000783627886
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-01-28 20:12:40 +0530

    Time: 10 ms (46.23%) | Memory: 11.5 MB (25.48%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -9,23 +9,58 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-    if(head==NULL)
-      return NULL;
-   unordered_set<ListNode *>s;
-  ListNode *temp=head;
+// brute force
+//     if(head==NULL)
+//       return NULL;
+//    unordered_set<ListNode *>s;
+//   ListNode *temp=head;
  
-  while(temp->next!=NULL)
-  {
-    if(s.find(temp)!=s.end())
-    {
-        return temp;
-    }
-    s.insert(temp);
-    temp=temp->next;
+//   while(temp->next!=NULL)
+//   {
+//     if(s.find(temp)!=s.end())
+//     {
+//         return temp;
+//     }
+//     s.insert(temp);
+//     temp=temp->next;
     
-  }
+//   }
 
-  return NULL;
+//   return NULL;
+
+ListNode *fast=head;
+ListNode *slow=head;
+if(head==NULL)
+return NULL;
+
+while(fast!=NULL&&fast->next!=NULL)
+{
+   fast=fast->next->next;
+   slow=slow->next;
+   if(fast==slow)
+   {
+    slow=head;
+    if(fast==slow)
+    return slow;
+    break;
+
+   }
+
+}
+
+
+
+while(fast!=NULL&&fast->next!=NULL)
+{
+    slow=slow->next;
+    fast=fast->next;
+    if(fast==slow)
+    {
+        return slow;
+    }
+}
+
+return NULL;
    
 
     }
