e21966dc6d1e7d0c2dc32a98bdb6d425b9a59f7d
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-02-01 22:42:38 +0530

    Time: 11 ms (46.94%) | Memory: 65.1 MB (41.24%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,34 +1,49 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int i=0;
 
-        while(i<nums.size())
-        {
-             if((nums[i]-1)!=i)
-             {
-                if(nums[i]!=nums[nums[i]-1])
-               swap(nums[i],nums[nums[i]-1]);
-               else i++;
-             }
-             else
-             {
-                i++;
-             }
+      //  TC-: o(2n) and its also modifiying an array
+    //     int i=0;
 
+    //     while(i<nums.size())
+    //     {
+    //          if((nums[i]-1)!=i)
+    //          {
+    //             if(nums[i]!=nums[nums[i]-1])
+    //            swap(nums[i],nums[nums[i]-1]);
+    //            else i++;
+    //          }
+    //          else
+    //          {
+    //             i++;
+    //          }
+    //  }
 
-        }
-
-        for(int i=0;i<nums.size();i++)
-        {
-            if((nums[i]-1)!=i)
-            {
-                return nums[i];
-            }
-        }
-
-    return 0;
+    //     for(int i=0;i<nums.size();i++)
+    //     {
+    //         if((nums[i]-1)!=i)
+    //         {
+    //             return nums[i];
+    //         }
+    //     }
 
 
+    // return 0;
+
+
+          int slow=nums[0];
+          int fast=nums[0];
+
+          do{
+           slow=nums[slow];
+           fast=nums[nums[fast]];
+          }while(slow!=fast);
+           slow=nums[0];
+           while(slow!=fast)
+           {
+            slow=nums[slow];
+            fast=nums[fast];
+           }  
+          return slow;
     }
 };
