8197dd1d70fa19a2c15de2c0db4bfd05ce430d77
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-02-02 20:27:33 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (51.10%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -2,24 +2,65 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         
-     set<int>s;
-     for(int i=0;i<nums.size();i++)
-     {
-        s.insert(nums[i]);
-     }
-     int j=0;
-    for(auto i:s)
-    {
-        nums[j++]=i;
-    }
+    //  set<int>s;
+    //  for(int i=0;i<nums.size();i++)
+    //  {
+    //     s.insert(nums[i]);
+    //  }
+    //  int j=0;
+    // for(auto i:s)
+    // {
+    //     nums[j++]=i;
+    // }
 
-    for(int i=j;i<nums.size();i++)
-    {
-        nums[i]=-1;
-    }
+    // for(int i=j;i<nums.size();i++)
+    // {
+    //     nums[i]=-101
+    //     ;
+    // }
  
 
-     return s.size();
+    //  return s.size();
+int i=0,j=0;
+int n=nums.size();
+bool b=false;
+int count=0;
+while(i<n)
+{
+    if(i-1>=0&&nums[i-1]==nums[i])
+     {
+             while(i<n&&nums[i]==nums[i-1])
+             {
+                i++;
+             }
+             if(i<n)
+             {
+                nums[j]=nums[i];
+                i++,j++;
+                count++;
+             }
+             b=true;
+
+     }
+     else
+     {
+        if(!b)
+        {
+            count++;
+            i++,j++;
+            
+        }
+        else
+        {
+            nums[j]=nums[i];
+            i++,j++;
+            count++;
+        }
+     }  
+
+}
+     
+     return count;
 
 
     }
