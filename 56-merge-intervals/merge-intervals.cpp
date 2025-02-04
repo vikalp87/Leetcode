ca44383c5ff173edad90413d52b7719022792005
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-02-04 21:41:10 +0530

    Time: 5 ms (57.21%) | Memory: 23.6 MB (92.04%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -6,21 +6,15 @@ public:
      ans.push_back(intervals[0]);
      for(int i=1;i<intervals.size();i++)
     {
-        vector<int>p=ans[ans.size()-1];
-        vector<int>p1=intervals[i];
-           if(p[1]>=p1[0])
-           {
-                 if(p[1]<=p1[1])
-                 {
-                    p[1]=p1[1];        
-                  }
+                if(ans.back()[1]>=intervals[i][0])
+                {
+                     ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+                }
+                else
+                {
+                    ans.push_back(intervals[i]);
+                }  
                 
-               ans[ans.size()-1]=p;         
-           }
-           else
-           {
-             ans.push_back(intervals[i]);
-           }
     }
   return ans;
     }
