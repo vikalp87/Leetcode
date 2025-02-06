bab663344654564c175382161fbd1a886b146918
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-02-06 22:42:01 +0530

    Time: 23 ms (33.24%) | Memory: 32.2 MB (47.35%) - LeetSync

--- 436-find-right-interval/find-right-interval.cpp
+++ 436-find-right-interval/find-right-interval.cpp
@@ -1,32 +1,54 @@
 class Solution {
 public:
     vector<int> findRightInterval(vector<vector<int>>& intervals) {
-        vector<int>ans;
-         
-        for(int i=0;i<intervals.size();i++)
+       
+       
+        // for(int i=0;i<intervals.size();i++)
+        // {
+        //     int index=-1;
+        //     int mini=INT_MAX;
+        //     for(int j=0;j<intervals.size();j++)
+        //     {
+                
+        //          if(intervals[j][0]>=intervals[i][1])
+        //         {
+                        
+        //                 if(mini>intervals[j][0])
+        //                 {
+        //                     mini=intervals[j][0];
+        //                     index=j;
+                        
+                        
+        //                 }
+        //             }
+                
+                
+                
+        //     }
+        //     ans.push_back(index);
+        // }
+
+        
+        
+            map<int,int>m;
+         int n=intervals.size();
+        
+        for(int i=0;i<n;i++)
         {
-            int index=-1;
-            int mini=INT_MAX;
-            for(int j=0;j<intervals.size();j++)
-            {
-                
-                 if(intervals[j][0]>=intervals[i][1])
-                {
-                        
-                        if(mini>intervals[j][0])
-                        {
-                            mini=intervals[j][0];
-                            index=j;
-                        
-                        
-                        }
-                    }
-                
-                
-                
-            }
-            ans.push_back(index);
+            m[intervals[i][0]]=i;
         }
+        sort(intervals.begin(),intervals.end());
+       
+       
+            vector<int>ans(n,-1);
+            for(int i=0;i<intervals.size();i++)
+            {
+                auto it=m.lower_bound(intervals[i][1]);
+                
+                if(it!=m.end())
+               ans[m[intervals[i][0]]]=it->second;
+            }
+       
      return ans;   
     }
 };
