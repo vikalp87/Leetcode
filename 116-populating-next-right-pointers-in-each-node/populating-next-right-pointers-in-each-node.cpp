5c2cb001a8d4a16be500553412095a730704461d
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-03-20 00:41:17 +0530

    Time: 10 ms (87.20%) | Memory: 19 MB (60.69%) - LeetSync

---
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,53 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (!root) {
+            return root;
+        }
+
+        queue<Node*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int n = q.size();
+            Node * linkAddress = q.front();
+
+            for (int i = 0; i < n; i++) {
+                Node* node = q.front();
+               
+               if(linkAddress!=node)
+                    node->next = linkAddress;
+                
+               
+
+                if (node->right != NULL) {
+                    q.push(node->right);
+                }
+                if (node->right != NULL) {
+                    q.push(node->left);
+                }
+                 linkAddress = node;
+                 q.pop();
+            }
+        }
+        return root;
+    }
+};
