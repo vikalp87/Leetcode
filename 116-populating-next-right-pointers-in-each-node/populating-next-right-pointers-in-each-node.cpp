060d41844729edce612ebdc225f85d875b0576e2
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-03-21 23:10:05 +0530

    Time: 16 ms (32.77%) | Memory: 18.9 MB (77.43%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -19,35 +19,50 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
-        if (!root) {
-            return root;
-        }
+        // if (!root) {
+        //     return root;
+        // }
 
-        queue<Node*> q;
-        q.push(root);
+        // queue<Node*> q;
+        // q.push(root);
 
-        while (!q.empty()) {
-            int n = q.size();
-            Node * linkAddress = q.front();
+        // while (!q.empty()) {
+        //     int n = q.size();
+        //     Node * linkAddress = q.front();
 
-            for (int i = 0; i < n; i++) {
-                Node* node = q.front();
+        //     for (int i = 0; i < n; i++) {
+        //         Node* node = q.front();
                
-               if(linkAddress!=node)
-                    node->next = linkAddress;
+        //        if(linkAddress!=node)
+        //             node->next = linkAddress;
                 
                
 
-                if (node->right != NULL) {
-                    q.push(node->right);
-                }
-                if (node->right != NULL) {
-                    q.push(node->left);
-                }
-                 linkAddress = node;
-                 q.pop();
-            }
+        //         if (node->right != NULL) {
+        //             q.push(node->right);
+        //         }
+        //         if (node->right != NULL) {
+        //             q.push(node->left);
+        //         }
+        //          linkAddress = node;
+        //          q.pop();
+        //     }
+        // }
+        // return root;
+
+        if(!root){
+            return root;
         }
+        if(root->left!=NULL&&root->right!=NULL){
+            root->left->next=root->right;
+            if(root->next!=NULL)
+            root->right->next=root->next->left;
+        }
+        connect(root->left);
+        connect (root->right);
         return root;
+
+
+
     }
 };
