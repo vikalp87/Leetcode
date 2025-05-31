991ccf9fc17f6f3acf3e55c3b122c4f118f34465
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-05-31 23:15:06 +0530

    Time: 3 ms (98.23%) | Memory: 11.3 MB (82.93%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -9,59 +9,31 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-// brute force
-//     if(head==NULL)
-//       return NULL;
-//    unordered_set<ListNode *>s;
-//   ListNode *temp=head;
- 
-//   while(temp->next!=NULL)
-//   {
-//     if(s.find(temp)!=s.end())
-//     {
-//         return temp;
-//     }
-//     s.insert(temp);
-//     temp=temp->next;
-    
-//   }
-
-//   return NULL;
-
-ListNode *fast=head;
-ListNode *slow=head;
-if(head==NULL)
-return NULL;
-
-while(fast!=NULL&&fast->next!=NULL)
-{
-   fast=fast->next->next;
-   slow=slow->next;
-   if(fast==slow)
-   {
-    slow=head;
-    if(fast==slow)
-    return slow;
-    while(slow!=fast)
-{
-    slow=slow->next;
-    fast=fast->next;
-    if(fast==slow)
-    {
-        return slow;
+  ListNode *fast=head;
+ ListNode *slow=head;
+if(head==NULL){
+    return NULL;
+}
+ while(fast!=NULL && fast->next!=NULL){
+       fast=fast->next->next;
+        slow=slow->next;
+        if(fast==slow){
+            slow=head;
+            break;
+        }
     }
-}
 
-   }
+    if(fast==NULL || fast->next==NULL){
+        return NULL;
+    }
 
-}
+    while(slow!=fast){
+        slow=slow->next;
+        fast=fast->next;
+    }
+    
+    return slow;
 
-
-
-
-
-return NULL;
-   
 
     }
 };
