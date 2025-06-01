5a4d00101d9082c33ae51a9526d5c2525db4da6d
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-06-01 19:45:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (97.38%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,29 +1,51 @@
 class Solution {
 public:
+ int sumOfSquares(int n) {
+    int sum = 0;
+    while (n > 0) {
+        int d = n % 10;
+        sum += d * d;
+        n /= 10;
+    }
+    return sum;
+}
     bool isHappy(int n) {
-        unordered_map<int, int> m;
-        int squares = 0;
-        m[n]++;
-        while (n != 0) {
-            int remainder = n % 10;
-            n = n / 10;
-            squares += pow(remainder, 2);
+        // unordered_map<int, int> m;
+     
+        // int squares = 0;
+       
+        // m[n]++;
+        // while (n != 0) {
+        //     int remainder = n % 10;
+        //     n = n / 10;
+        //     squares += pow(remainder, 2);
 
-            if (n == 0) {
+        //     if (n == 0) {
+               
+        //       if (squares == 1) {
+        //             return true;
+        //         }
 
-                if (squares == 1) {
-                    return true;
-                }
+        //         if (m.find(squares) != m.end()) {
+        //             return false;
+        //         }
+        //         m[squares]++;
 
-                if (m.find(squares) != m.end()) {
-                    return false;
-                }
-                m[squares]++;
+        //         n = squares;
+        //         squares = 0;
+        //     }
+        // }
+        // return false;
 
-                n = squares;
-                squares = 0;
-            }
-        }
-        return false;
+        int slow=n,fast=n;
+       while(fast!=1){
+        slow=sumOfSquares(slow);
+        fast=sumOfSquares(sumOfSquares(fast));
+
+        if(fast==slow && fast !=1)return false;
+
+      }
+
+     return true;
     }
 };
