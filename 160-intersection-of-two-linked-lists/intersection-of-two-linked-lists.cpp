b654c19223afa9659a29709b818fede3869a95ae
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-06-09 00:35:43 +0530

    Time: 34 ms (77.59%) | Memory: 18.7 MB (24.24%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,24 +8,74 @@
  */
 class Solution {
 public:
+
+      ListNode * distance(ListNode *head,int k){
+           ListNode *temp=head;
+           for(int i=0;i<k;i++){
+            temp=temp->next;
+           }
+           return temp;
+      }
+
+      ListNode *find(ListNode*list1,ListNode *list2){
+        while(list1!=list2){
+            list1=list1->next;
+            list2=list2->next;
+        }
+        return list1?list1:NULL;
+
+      }
+
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
 
-          ListNode *temp=headA;
+//           ListNode *temp=headA;
+//           ListNode *ptr2=headB;
+//           unordered_set<ListNode *>set;
+//           while(temp!=NULL){
+//             set.insert(temp);
+//             temp=temp->next;
+//           }
+
+//           while(ptr2!=NULL){
+          
+//           if(set.find(ptr2)!=set.end()){
+//             return ptr2;
+//           }
+
+//             ptr2=ptr2->next;
+//           }
+// return NULL;
+
+
+         ListNode *temp=headA;
           ListNode *ptr2=headB;
-          unordered_set<ListNode *>set;
+
+          int length1=0,length2=0;
+      
           while(temp!=NULL){
-            set.insert(temp);
-            temp=temp->next;
+           temp=temp->next;
+           length1++;
           }
 
           while(ptr2!=NULL){
-          
-          if(set.find(ptr2)!=set.end()){
-            return ptr2;
+             ptr2=ptr2->next;
+             length2++;
           }
 
-            ptr2=ptr2->next;
-          }
+          
+       int difference=abs(length1-length2);
+        ListNode *move;
+         if(length1>=length2){
+            move=distance(headA,difference);
+           return find(move,headB);
+
+         }
+         else if(length1<length2){
+             move=distance(headB,difference);
+            return  find(headA,move);
+         }
+
+         
 return NULL;
 
     }
