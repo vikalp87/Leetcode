40ce2c373ee17382e53fe0bd4c51b3b3cac090ad
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-06-08 21:37:34 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (38.90%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -10,37 +10,57 @@
  */
 class Solution {
 public:
+    pair<ListNode*, ListNode*> reverseList(ListNode* fillNode, int k) {
 
-   ListNode * reverseList(vector<int>&ans,ListNode *fillNode){
-     
-      for(int i=ans.size()-1;i>=0;i--){
-       
-        fillNode->val=ans[i];
-        fillNode=fillNode->next;
-      }
-      return fillNode;
-
+        ListNode* current = fillNode;
+        ListNode* previous = NULL;
+        ListNode* temp = fillNode;
+        while (current != NULL && k--) {
+            current = current->next;
+            temp->next = previous;
+            previous = temp;
+            temp = current;
+        }
+        return {previous, fillNode};
     }
 
     ListNode* reverseKGroup(ListNode* head, int k) {
-        vector<int>ans;
-        ListNode *currentNode=head;
-        ListNode *fillNode=head;
 
-        if(head==NULL || head->next==NULL|| k==1){
+        ListNode* currentNode = head;
+        ListNode* fillNode = head;
+        ListNode *previousGrpTail=NULL;
+
+        int count = 0;
+        if (head == NULL || head->next == NULL || k == 1) {
             return head;
         }
-         
-        while(currentNode!=NULL){
-          
-        ans.push_back(currentNode->val);
-         currentNode=currentNode->next;
-         
-           if(ans.size()==k){
-           fillNode=reverseList(ans,fillNode);
-            ans.clear();
-           }
-     }
-           return head;
+
+        while (currentNode != NULL) {
+
+            count++;
+            currentNode = currentNode->next;
+
+            if (count == k) {
+                pair<ListNode*,ListNode *>data=reverseList(fillNode, k);
+                fillNode = currentNode;
+               
+               if(previousGrpTail!=NULL){
+                  previousGrpTail->next=data.first;
+               }
+               else{
+                head=data.first;
+               }
+               
+                 previousGrpTail=data.second;
+                  count = 0;
+            }
+
+           
+        }
+
+       if(previousGrpTail!=NULL)
+          previousGrpTail->next=fillNode;
+       
+        return head;
     }
 };
