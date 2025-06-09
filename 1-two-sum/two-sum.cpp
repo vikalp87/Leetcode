ee6307151f2883c67bebec7493631d8fe248bb43
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-06-09 23:43:22 +0530

    Time: 71 ms (17.19%) | Memory: 14 MB (93.08%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,64 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    //     vector<int>ans;
-    //     ans=nums;
-    //    sort(ans.begin(),ans.end());
-    //    int left=0,right=ans.size()-1;
-    //    int a,b;
-       
-    //    while(left<right)
-    //    {
-    //        if(ans[left]+ans[right]==target)
-    //        {
-    //             a=ans[left];
-    //             b=ans[right];
-    //             break;  
-    //        }
-    //        else if(ans[left]+ans[right]>target)
-    //        {
-    //              right--;
-    //        }
-    //        else
-    //        {
-    //                left++;
-    //        }
-    //    }
-    
-    // ans.clear();
-    // for(int i=0;i<nums.size();i++)
-    // {
-    //     if(a==nums[i])
-    //     {
-    //       ans.push_back(i);
-    //     }
-    //     else if(b==nums[i])
-    //     {
-    //         ans.push_back(i);
-    //     }
-    //     else
-    //     continue;
-
-
-    // }
-
-    // return ans;
-
-
-    //hashinh
-    map<int,int>m;
-    vector<int>ans;
-    for(int i=0;i<nums.size();i++)
-    {
-        int x=target-nums[i];
-        if(m.find(x)!=m.end())
-        {
-            ans.push_back(m[x]);
-            ans.push_back(i);
-
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    return ans;
+                }
+            }
         }
-        m[nums[i]]=i;
-    }
-    return ans;
+        return ans;
     }
 };
