bbd10595d2183161ec21475709a6efaf0255b86f
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-06-16 00:50:09 +0530

    Time: 22 ms (5.75%) | Memory: 60.7 MB (5.90%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool ship(int mid, vector<int> weights, int days) {
+
+        int totalDays = 0;
+        int sum=0;
+
+        for (int i = 0; i < weights.size(); i++) {
+
+            sum+=weights[i];
+            if(sum>mid){
+                totalDays++;
+                sum=weights[i];
+            }
+        }
+
+        if(sum<=mid){
+            totalDays++;
+        }
+
+        cout<<mid<<" "<<totalDays<<endl;
+
+        if (totalDays <= days)
+            return true;
+        return false;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {
+
+        int left =* max_element(weights.begin(), weights.end());
+        int right = 0;
+
+        for (int i = 0; i < weights.size(); i++) {
+            right += weights[i];
+        }
+
+        while (left < right) {
+
+            int mid = left + ((right - left) / 2);
+
+            if (ship(mid, weights, days)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+};
