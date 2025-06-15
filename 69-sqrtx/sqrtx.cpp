a894fbde0ed9264999e68dfce50b0fd46e542e61
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-06-15 23:03:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (99.28%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -2,19 +2,38 @@ class Solution {
 public:
     int mySqrt(int x) {
 
-        if(x==0|| x==1)return x;
+//         if(x==0|| x==1)return x;
     
-        for(int i=1;i<=x;i++){
-             long long pow=i;
-              pow=pow*i;
+//         for(int i=1;i<=x;i++){
+//              long long pow=i;
+//               pow=pow*i;
+//             if(pow==x){
+//                 return i;
+//             }
+//             else if(pow>x){
+//                // no overflow
+//               return i-1;    
+//             }
+//         }
+// return 0;
+
+        int left=1,right=x;
+        while(left<=right){
+            int mid=left+((right-left)/2);
+             long long pow=mid;
+             pow*=mid;
+
             if(pow==x){
-                return i;
+                return mid;
             }
-            else if(pow>x){
-               // no overflow
-              return i-1;    
+            else if(pow<x){
+                  left=mid+1;
+            }
+            else{
+                  right=mid-1;
             }
         }
-return 0;
+return left-1;
+
     }
 };
