bb6209bde31977dba9f237155f06f666bb7ab51d
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-06-15 22:14:49 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (69.80%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -6,17 +6,17 @@ public:
     int firstBadVersion(int n) {
         
        int low=1, high=n;
-       int ans;
-       while(low<=high){
+
+       while(low<high){
         int mid=low+((high-low)/2);
         if(isBadVersion(mid)){
-            ans=mid;
-            high=mid-1;
+          high=mid;
         }
         else{
             low=mid+1;
         }
        }
-       return ans;
+       return low;
+      
     }
 };
