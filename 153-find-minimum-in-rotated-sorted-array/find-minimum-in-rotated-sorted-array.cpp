fd3aff7631a1b369ef9cfe59535db78a02985b52
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-06-18 00:30:07 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (48.80%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -16,7 +16,7 @@ public:
                 low = mid + 1;
             } else {
                 ans = min(ans, nums[mid]);
-                high = mid;
+                high = mid-1;
             }
         }
         return ans;
