353f6f567fcf640f17bfebe362274c5185d728ab
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-07-02 22:36:07 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (34.33%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -6,29 +6,27 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    bool pathSum(TreeNode* root, int targetSum, int& sum) {
+      bool pathsum(TreeNode *root,int targetSum, int sum){
+          if(!root)return false;
+          sum+=root->val;  
+          if(root->left==NULL && root->right==NULL && sum==targetSum){
+              return true;
+              sum=sum-root->val;
+          }
 
-        if (root == NULL) {
-            return false;
-        }
-        sum += root->val;
-        if (root->left == NULL && root->right == NULL&&sum==targetSum) {
+            bool isLeft=pathsum(root->left,targetSum,sum);
+            bool isRight=pathsum(root->right,targetSum,sum);
+
             sum=sum-root->val;
-            return true;
-        }
-        bool a = pathSum(root->left, targetSum, sum);
-        bool b = pathSum(root->right, targetSum, sum);
-        sum = sum - root->val;
-        return a||b;
-    }
+            return isLeft || isRight;
+            
+      }
     bool hasPathSum(TreeNode* root, int targetSum) {
-        int sum = 0;
-        return pathSum(root, targetSum, sum);
+      return pathsum(root,targetSum,0);
     }
 };
