8f68814f17a0318b739274f0ebe1828cc8467f86
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-07-02 23:18:18 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (98.93%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -12,31 +12,28 @@
  */
 class Solution {
 public:
-    void pathsum(TreeNode* root, int targetSum,vector<int>ans,  vector<vector<int>>&result) {
-        if (!root)
-            return ;
+    void pathsum(TreeNode* root, int targetSum, vector<int>& ans,
+                 vector<vector<int>>& result) {
+        if (!root) return;
         targetSum = targetSum - root->val;
         ans.push_back(root->val);
         if (root->left == NULL && root->right == NULL) {
-           if(targetSum==0){
-            result.push_back(ans);
-            return ;
+            if (targetSum == 0) {
+                result.push_back(ans);
             }
-            return ;
+            ans.pop_back();
+            return;
         }
 
-         pathsum(root->left, targetSum,ans,result);
-         pathsum(root->right, targetSum,ans,result);
-
-
+        pathsum(root->left, targetSum, ans, result);
+        pathsum(root->right, targetSum, ans, result);
+        ans.pop_back();
     }
 
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<int>ans;
-        vector<vector<int>>result;
-        pathsum(root,targetSum,ans,result);
+        vector<int> ans;
+        vector<vector<int>> result;
+        pathsum(root, targetSum, ans, result);
         return result;
-
-
     }
 };
