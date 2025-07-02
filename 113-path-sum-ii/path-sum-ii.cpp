1c905a2a2fdca2a8388254bf5be8735c6ffd4564
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-07-02 23:00:45 +0530

    Time: 17 ms (15.19%) | Memory: 38.9 MB (24.92%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -12,29 +12,31 @@
  */
 class Solution {
 public:
-    void pathSum(TreeNode* root, int targetSum, vector<int>& tempres,
-                 vector<vector<int>>& ans) {
-        if (!root) {
-            return;
+    bool pathsum(TreeNode* root, int targetSum,vector<int>ans,  vector<vector<int>>&result) {
+        if (!root)
+            return false;
+        targetSum = targetSum - root->val;
+        ans.push_back(root->val);
+        if (root->left == NULL && root->right == NULL) {
+           if(targetSum==0){
+            result.push_back(ans);
+            return true;
+            }
+            return false;
         }
 
-        tempres.push_back(root->val);
-        int remainingSum = targetSum - root->val;
-        if (root->left == NULL && root->right == NULL) {
-            if (remainingSum == 0) {
-                ans.push_back(tempres);
-             } 
-             tempres.pop_back();
-                return;
-        }
-        pathSum(root->left, remainingSum, tempres, ans);
-        pathSum(root->right, remainingSum, tempres, ans);
-        tempres.pop_back();
+        bool isLeft = pathsum(root->left, targetSum,ans,result);
+        bool isRight = pathsum(root->right, targetSum,ans,result);
+
+        return isLeft || isRight;
     }
+
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<vector<int>> ans;
-        vector<int> tempres;
-        pathSum(root, targetSum, tempres, ans);
-        return ans;
+        vector<int>ans;
+        vector<vector<int>>result;
+        pathsum(root,targetSum,ans,result);
+        return result;
+
+
     }
 };
