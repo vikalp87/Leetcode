dabe81fca0eb5bda13fc0a43a62bf5bf18816f0f
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-07-07 22:29:00 +0530

    Time: 4 ms (57.03%) | Memory: 27.6 MB (20.61%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -12,34 +12,31 @@
  */
 class Solution {
 public:
-    int findLeftAndRightPart(int key, vector<int>& inorder, int i, int j) {
-        for (int k = i; k <= j; k++) {
-
-            if (inorder[k] == key) {
-                return k;
-            }
-        }
-        return -1;
-    }
-
     TreeNode* helper(vector<int>& preorder, vector<int>& inorder, int& index,
-                     int i, int j) {
+                     int i, int j, unordered_map<int, int>& m) {
 
         if (i > j)
             return NULL;
 
         TreeNode* newNode = new TreeNode(preorder[index]);
 
-        int indexValue = findLeftAndRightPart(preorder[index], inorder, i, j);
-        index++;
-        newNode->left = helper(preorder, inorder, index, i, indexValue - 1);
-        newNode->right = helper(preorder, inorder, index, indexValue + 1, j);
+        int indexValue = m[preorder[index++]];
+
+        newNode->left = helper(preorder, inorder, index, i, indexValue - 1, m);
+        newNode->right = helper(preorder, inorder, index, indexValue + 1, j, m);
         return newNode;
     }
 
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int i = 0, j = inorder.size() - 1;
         int index = 0;
-        return helper(preorder, inorder, index, i, j);
+
+        unordered_map<int, int> inorderMap;
+
+        for (int i = 0; i < inorder.size(); i++) {
+            inorderMap[inorder[i]] = i;
+        }
+
+        return helper(preorder, inorder, index, i, j, inorderMap);
     }
 };
