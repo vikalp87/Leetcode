09318575a7df0222bbaf70f45bf1c91f583ea91e
Vikalp Jain <72161234+vikalp87@users.noreply.github.com>
2025-07-17 23:33:37 +0530

    Time: 47 ms (58.32%) | Memory: 45.5 MB (44.81%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,53 +1,21 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        //it is only for +ve numbers
-        // int i=0,j=0;
-        // int count=0;
-        // int sum=0;
-        // while(j<nums.size())
-        // {
-        //     sum+=nums[j];
-        //     if(sum<k)
-        //     j++;
-        //     else if(sum==k)
-        //     {
-        //     count++;
-        //     j++;
-        //     }
-        //     else
-        //     {
-        //         while(sum>k)
-        //         {
-        //             sum-=nums[i];
-        //             i++;
-                   
-        //         }
-        //         if(sum==k)
-        //         {
-        //         count++;
-        //         }
-        //         j++;
-                
-        //     }
-        // }
-        // return count;
-    int n=nums.size();
-    int count=0;
-    unordered_map<int,int>m;
-    m[0]=1;
-    int prefixsum=0;
-     for(int i=0;i<n;i++)
-     {
-        prefixsum+=nums[i];
-       int searchNo=prefixsum-k;
-        if(m.find(searchNo)!=m.end())
-        {
-            count=count+m[searchNo];
-        }
-        m[prefixsum]++;
-     }
+        unordered_map<int,int>m;
+        int count=0;
+        m[0]=1;
+        int prefixsum=0;
+        
+        // if an particular index subarray is our ans at index j then x-k must be presnt in map
+        for(int i=0;i<nums.size();i++){
+           prefixsum+=nums[i];
+           
+           if(m.find(prefixsum-k)!=m.end()){
+              count+=m[prefixsum-k];
+           }           
 
-   return count;
+            m[prefixsum]++;
+        }
+        return count;
     }
 };
